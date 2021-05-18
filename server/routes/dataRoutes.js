fae88c04f22f340d523198619c938c3fd9eaebe5
Abhinav Singh <thakurabhinav17122001@gmail.com>
2021-05-18 21:42:00 +0530

    frontend

--- server/routes/dataRoutes.js
+++
@@ -1,9 +0,0 @@
-const router=require('express').Router()
-const { protect } = require('../middleware/authMiddleware')
-const {addData,getAllData,getData,deleteData}=require('../controllers/dataController') 
-
-router.route('/').post(protect,addData)
-router.route('/').get(protect,getAllData)
-router.route('/:id').get(protect,getData)
-router.route('/:id').delete(protect,deleteData)
-module.exports=router
--- server/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const router=require('express').Router()
-const {authUser,registerUser} = require('../controllers/userController')
-const { protect } = require('../middleware/authMiddleware')
-
-router.post('/login',authUser)
-router.route('/').post(registerUser)
-
-module.exports=router
